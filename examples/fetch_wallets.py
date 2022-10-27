eee39e5f88227aced774ac252581b97228c2b275
Frank Odongkara <fodongkara@gmail.com>
2022-10-27 12:43:02 +0300

    Test feedback

--- examples/fetch_wallets.py
+++
@@ -1,7 +0,0 @@
-from eversend import Eversend
-
-if __name__ == '__main__':
-    evs = Eversend(
-        'mbn3tFzJuJ7F4lP1dzuTdrsczhMXnGBR',
-        'HNoe-jlzPL2VuDAbdeD4h1I_otiLm0X7yTDVu_77WkowXyLqGnXn0T7MOkhzyhjZ')
-    evs.Wallets.list()
